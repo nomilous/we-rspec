5a9e329012d1048a460f18de761d970d2c9ea091
richardemslie <richard@clue.co.za>
2012-10-28 15:28:57 +0200

    forgot to test local linker

--- spec/linker_spec.rb
+++
@@ -1,7 +0,0 @@
-we enable: :linking
-
-we document: 'To link' do
-
-  we link: 'linkee'
-
-end
--- spec/we-rspec/link_spec.rb
+++ spec/we-rspec/link_spec.rb
@@ -1,5 +1,17 @@
 describe 'We::Rspec::link' do
 
-  it 'links'
+  it 'require the linked fragment' do
+
+    we enable: :linking
+
+    expect {
+
+      We::RSpec::Link.new.inject( link: 'controllers/hydraulics' )
+
+    }.to raise_error LoadError, 
+
+    /file -- controllers\/hydraulics_spec.rb/
+
+  end
 
 end
