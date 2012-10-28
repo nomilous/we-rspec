612480710245e4340f514df9322c612767dc0d0d
richardemslie <richard@clue.co.za>
2012-10-28 21:19:09 +0200

    wrap into a document node

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,45 +1,51 @@
-describe 'Results' do
+require 'config/rspec'  # guard is doing this
 
-  it 'has a pending block test'
+we document: 'Test Results' do
 
-  it 'has a pending nested test' do
-    pending 'Reason test pending'
-  end
+  describe 'Results' do
 
-  it 'has a test that fails' do
-    true.should == false
-  end
+    it 'has a pending block test'
 
-  it 'has a test that passes' do
-    true.should_not == false
-  end
+    it 'has a pending nested test' do
+      pending 'Reason test pending'
+    end
 
-  
-  context 'has a context with' do
-
-    it 'an in context failing test' do
+    it 'has a test that fails' do
       true.should == false
     end
 
-    it 'an in context passing test' do
-      true.should == true
+    it 'has a test that passes' do
+      true.should_not == false
     end
 
-    it 'an in context pending test' do
-      pending 'Reason in context test pending'
-    end
+    
+    context 'has a context with' do
 
-    xit 'a disabled test' do
-      true.should == false
-    end
+      it 'an in context failing test' do
+        true.should == false
+      end
 
-    it 'a test on line 36' do
-      #
-      # that a developer is running standalone with:
-      #  
-      #  rspec spec/test_spec.rb:36 --color
-      #
-      true.should == false
+      it 'an in context passing test' do
+        true.should == true
+      end
+
+      it 'an in context pending test' do
+        pending 'Reason in context test pending'
+      end
+
+      xit 'a disabled test' do
+        true.should == false
+      end
+
+      it 'a test on line 36' do
+        #
+        # that a developer is running standalone with:
+        #  
+        #  rspec spec/test_spec.rb:36 --colour
+        #
+        true.should == false
+      end
+
     end
 
   end
