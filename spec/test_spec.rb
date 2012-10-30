f59886cb7b3d1a996bc2f6840439ba213e449a0d
richardemslie <richard@clue.co.za>
2012-10-31 00:40:29 +0200

    .

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,53 +1,11 @@
 require 'config/rspec'  # guard is doing this
 
-we component: 'Test Results' do
-
-  describe 'Results' do
-
-    it 'has a pending block test'
-
-    it 'has a pending nested test' do
-      pending 'Reason test pending'
-    end
-
-    it 'has a test that fails' do
-      true.should == false
-    end
-
-    it 'has a test that passes' do
-      true.should_not == false
-    end
-
+describe 'Results' do
     
-    context 'has a context with' do
+    it 'has a test' do
 
-      it 'an in context failing test' do
-        true.should == false
-      end
-
-      it 'an in context passing test' do
-        true.should == true
-      end
-
-      it 'an in context pending test' do
-        pending 'Reason in context test pending'
-      end
-
-      xit 'a disabled test' do
-        true.should == false
-      end
-
-      it 'a test on line 36' do
-        #
-        # that a developer is running standalone with:
-        #  
-        #  rspec spec/test_spec.rb:36 --colour
-        #
-        true.should == false
-      end
+      true.should == false
 
     end
-
-  end
 
 end
