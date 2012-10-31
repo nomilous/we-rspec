aa7476259a6a2bd2776bcdbccbab28cc177a83bf
richardemslie <richard@clue.co.za>
2012-10-31 23:33:06 +0200

    okgood

--- lib/we/rspec.rb
+++ lib/we/rspec.rb
@@ -3,25 +3,6 @@ require 'we-rspec/formatter'
 require 'we-rspec/link'
 require 'we-rspec/nodes'
 
-module We
-
-  module Event
-
-    def self.end
-
-    #
-    # override end to show tree
-    #
-
-      ap We::tree
-
-    end
-
-  end
-
-end
-
-
 #
 # Assign We::RSpec::Formatter to 
 # receive inline test results
